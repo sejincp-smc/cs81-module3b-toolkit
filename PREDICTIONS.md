bea2dc0a69146ad5cd495cc5c681533b27ae3d90
Sejin Park <park_sejin02@student.smc.edu>
2025-07-08 06:20:18 +0900

    Add 4 more functions and predictions

---
+++ PREDICTIONS.md
@@ -0,0 +1,11 @@
+# PREDICTIONS.md
+
+- double() will double 'n'.
+
+- square() will square 'n'.
+
+- isEven() will show 'n' is even or not.
+
+- isOdd() will show 'n' is odd or not.
+
+- multiply() will multiply 'a' and 'b'.
--- math.js
+++ math.js
@@ -1,3 +1,24 @@
+// double
 function double(n) {
   return n * 2;
+}
+
+// square
+function square(n) {
+  return n * n;
+}
+
+// isEven
+function isEven(n) {
+  return n % 2 === 0;
+}
+
+// isOdd
+function isOdd(n) {
+  return n % 2 !== 0;
+}
+
+// multiply
+function multiply(a, b) {
+  return a * b;
 }
